5a1f416ccc3f9616d7f698f19a060eb84400f5f9
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-06-21 02:42:31 +0530

    additions for lounge

--- admin/options.php
+++ admin/options.php
@@ -59,7 +59,7 @@
 <div id="page">
     <div id="header">
         <h1>Options </h1>
-        <a href="index.php">Home</a> | <a href="categories-menu.php">Categories</a> | <a href="foods-menu.php">Foods</a> | <a href="accounts.php">Accounts</a> | <a href="orders.php">Orders</a> | <a href="reservations.php">Reservations</a> | <a href="specials.php">Promotions</a> | <a href="allocation.php">Staff</a> | <a href="options.php">Options</a> | <a href="logout.php">Logout</a>
+        <a href="index.php">Home</a> | <a href="foods-menu.php">Foods</a> | <a href="accounts.php">Accounts</a> | <a href="orders.php">Orders</a> | <a href="reservations.php">Reservations</a> | <a href="specials.php">Promotions</a> | <a href="allocation.php">Staff</a> | <a href="options.php">Options</a> | <a href="logout.php">Logout</a>
     </div>
 
     <!-- Manage Categories -->
