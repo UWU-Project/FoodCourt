efb535e840098c4616d4d01356b8119f2a254605
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-07-13 09:56:29 +0530

    Done

--- cart/test2.php
+++ cart/test2.php
@@ -83,7 +83,7 @@ or die("A problem has occured ... \n" . "Our team is working on it at the moment
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script language="JavaScript" src="../validation/user.js"></script>
     <script src="https://use.fontawesome.com/c560c025cf.js"></script>
 
