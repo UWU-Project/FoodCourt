ea2d67d737920f368ab3115f5428e605d42bb94d
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-07-15 01:17:50 +0530

    Done

--- cart/billing-alternative.php
+++ cart/billing-alternative.php
@@ -5,87 +5,189 @@ require_once('../auth.php');
 require_once('../connection/config.php');
 
 //Connect to mysqli server
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_DATABASE);
-if(!$conn) {
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+if (!$conn) {
     die('Failed to connect to server: ' . mysqli_error());
 }
 
-
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+//selecting all records from almost all tables. Return an error if there are no records in the tables
+$result = mysqli_query($conn, "SELECT * FROM orders_details o inner join cart_details c on c.cart_id = o.cart_id inner join quantities q on q.quantity_id = c.quantity_id inner join customers m on m.member_id = c.member_id inner join billing_details b on b.billing_id = o.billing_id ") or die("There are no records to display ... \n" . mysqli_error());
+?>
+
+<!doctype html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo APP_NAME ?>:Alternative Billing</title>
-    <link href="stylesheets/user_styles.css" rel="stylesheet" type="text/css"/>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
+
     <script language="JavaScript" src="../validation/user.js">
     </script>
+    <style>
+        .container {
+            max-width: 960px;
+        }
+
+        .lh-condensed {
+            line-height: 1.25;
+        }
+    </style>
 </head>
 <body>
 <div id="page">
-    <div id="menu">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="foodzone.php">Food Zone</a></li>
-            <li><a href="specialdeals.php">Special Deals</a></li>
-            <li><a href="member-index.php">My Account</a></li>
-            <li><a href="contactus.php">Contact Us</a></li>
-        </ul>
-    </div>
+
     <div id="header">
         <div id="logo"><a href="index.php" class="blockLink"></a></div>
-        <div id="company_name"><?php echo APP_NAME ?> Restaurant</div>
+
+
     </div>
-    <div id="center">
-        <h1>Billing Address</h1>
-        <hr>
-        <p>We have found out that you don't have a billing address in your account. Please add a billing address in the
-            form below. It is the same address that will be used to deliver your food orders. Please note that ONLY
-            correct street/physical addresses should be used in order to ensure smooth delivery of your food orders. For
-            more information <a href="contactus.php">Click Here</a> to contact us.</p>
-        <div style="border:#bd6f2f solid 1px;padding:4px 6px 2px 6px">
-            <form id="billingForm" name="billingForm" method="post"
-                  action="billing-exec.php?id=<?php echo $_SESSION['SESS_MEMBER_ID']; ?>"
-                  onsubmit="return billingValidate(this)">
-                <table width="300" border="0" align="center" cellpadding="2" cellspacing="0">
-                    <CAPTION><h3>ADD DELIVERY/BILLING ADDRESS</h3></CAPTION>
-                    <tr>
-                        <td colspan="2" style="text-align:center;"> Required fields</td>
-                    </tr>
-                    <tr>
-                        <th>Street Address</th>
-                        <td> <input name="sAddress" type="text" class="textfield"
-                                    id="sAddress"/></td>
-                    </tr>
-                    <tr>
-                        <th>P.O. Box No</th>
-                        <td> <input name="box" type="text" class="textfield" id="box"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>City</th>
-                        <td> <input name="city" type="text" class="textfield" id="city"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th width="124">Mobile No</th>
-                        <td width="168"><input name="mNumber" type="text" class="textfield" id="mNumber"/></td>
-                    </tr>
-                    <tr>
-                        <th>Landline No</th>
-                        <td>&nbsp;&nbsp;&nbsp;<input name="lNumber" type="text" class="textfield" id="lNumber"/></td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td><input type="submit" name="Submit" value="Add"/></td>
-                    </tr>
-                </table>
-            </form>
+
+
+    <div class="container">
+        <div class="py-5 text-center">
+            <img class="d-block mx-auto mb-4" src="../images/logo2copy.png" alt="">
+            <h1>Billing Address</h1>
+            <hr>
+            <p>We have found out that you don't have a billing address in your account. Please add a billing address in
+                the
+                form below. It is the same address that will be used to deliver your food orders. Please note that ONLY
+                correct street/physical addresses should be used in order to ensure smooth delivery of your food orders.
+                For
+                more information <a href="contactus.php">Click Here</a> to contact us.</p></div>
+
+
+        <div class="row">
+            <div class="col-md-8 order-md-1">
+                <h4 class="mb-3">Billing address</h4>
+
+
+                <form id="billingForm" class="needs-validation" novalidate="" name="billingForm" method="post"
+                      action="billing-exec.php?id=<?php echo $_SESSION['SESS_MEMBER_ID']; ?>"
+                      onsubmit="return billingValidate(this)">
+                    <div class="row">
+                        <!--<div class="col-md-6 mb-3">
+                            <label for="firstName">First name</label>
+                            <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+                            <div class="invalid-feedback"> Valid first name is required. </div>
+                        </div>
+
+                        <div class="col-md-6 mb-3">
+                            <label for="lastName">Last name</label>
+                            <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
+                            <div class="invalid-feedback"> Valid last name is required. </div>
+                        </div>
+
+                    </div>
+
+                    <div class="mb-3">
+
+                        <label for="username">Username</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">@</span>
+                            </div>
+                            <input type="text" class="form-control" id="username" placeholder="Username" required="">
+                            <div class="invalid-feedback" style="width: 100%;"> Your username is required. </div>
+                        </div>
+                    </div>-->
+                        <div class="mb-3">
+                            <label for="address2">P.O. Box No <span class="text-muted">(Optional)</span></label>
+                            <input name="box" type="text" class="form-control" id="address2"
+                                   placeholder="Apartment or Suite" id="box">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="address">Street</label>
+                            <input name="sAddress" type="text" class="form-control" id="sAddress"
+                                   placeholder="Enter the Address" required="">
+                            <div class="invalid-feedback"> Please enter your shipping address.</div>
+                        </div>
+
+
+
+                        <div class="mb-3">
+                            <label for="address">City</label>
+                            <input name="city" id="city" type="text" class="form-control"
+                                   placeholder="Enter the Address" required="">
+                            <div class="invalid-feedback"> Please enter your shipping address.</div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4 mb-4">
+                                <label for="mNumber">Mobile No</label>
+                                <input name="mNumber" type="text" class="form-control" id="mNumber" placeholder=""
+                                       required="">
+                                <div class="invalid-feedback"> Mobile Number Required.</div>
+                            </div>
+
+                            <div class="col-md-4 mb-4">
+                                <label for="lNumber">Landline No<span class="text-muted">(Optional)</span></label>
+                                <input name="lNumber" type="text" class="form-control" id="lNumber"
+                                       placeholder="Apartment or Suite" >
+
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="same-address" required="">
+                            <label class="custom-control-label" for="same-address" >Shipping address is the same as my
+                                billing address</label>
+                            <div class="invalid-feedback"> Make Sure to Check</div>
+                        </div>
+                        <hr>
+
+
+                        <button class="btn btn-primary btn-lg btn-block" type="submit" name="Submit" value="Add">
+                            Save The Address
+                        </button>
+                    </div>
+
+                </form>
+            </div>
         </div>
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+            <div id="company_name"><?php echo APP_NAME ?> Restaurant</div>
+
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Privacy</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Support</a></li>
+            </ul>
+        </footer>
     </div>
-    <?php include 'footer.php'; ?>
-</div>
+
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function () {
+            'use strict'
+
+            window.addEventListener('load', function () {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation')
+
+                // Loop over them and prevent submission
+                Array.prototype.filter.call(forms, function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+            }, false)
+        }())
+    </script>
 </body>
 </html>
--- cart/billing-success.php
+++ cart/billing-success.php
@@ -1,34 +1,98 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php include '../connection/config.php'; ?>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <title><?php echo APP_NAME ?>: Billing Success</title>
-    <link href="stylesheets/user_styles.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://use.fontawesome.com/c560c025cf.js"></script>
+    <style>
+
+        body {
+            background: #f2f2f2;
+        }
+
+        .payment {
+            border: 1px solid #f2f2f2;
+            height: 280px;
+            border-radius: 20px;
+            background: #fff;
+        }
+
+        .payment_header {
+            background: rgba(255, 102, 0, 1);
+            padding: 20px;
+            border-radius: 20px 20px 0px 0px;
+
+        }
+
+        .check {
+            margin: 0px auto;
+            width: 50px;
+            height: 50px;
+            border-radius: 100%;
+            background: #fff;
+            text-align: center;
+        }
+
+        .check i {
+            vertical-align: middle;
+            line-height: 50px;
+            font-size: 30px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .content h1 {
+            font-size: 25px;
+            padding-top: 25px;
+        }
+
+        .content a {
+            width: 200px;
+            height: 35px;
+            color: #fff;
+            border-radius: 30px;
+            padding: 5px 10px;
+            background: rgba(255, 102, 0, 1);
+            transition: all ease-in-out 0.3s;
+        }
+
+        .content a:hover {
+            text-decoration: none;
+            background: #000;
+        }
+
+    </style
+    </style>
 </head>
 <body>
-<div id="page">
-    <div id="menu"><ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="foodzone.php">Food Zone</a></li>
-            <li><a href="specialdeals.php">Special Deals</a></li>
-            <li><a href="member-index.php">My Account</a></li>
-            <li><a href="contactus.php">Contact Us</a></li>
-        </ul>
-    </div>
-    <div id="header">
-        <div id="logo"> <a href="index.php" class="blockLink"></a></div>
-        <div id="company_name"><?php echo APP_NAME ?> Restaurant</div>
-    </div>
-    <div id="center">
-        <h1>Billing Successful</h1>
-        <div style="border:#bd6f2f solid 1px;padding:4px 6px 2px 6px">
-            <p>&nbsp;</p>
-            <div class="error">Billing Information Added Successfully!</div>
-            <p><a href="member-index.php">Click Here</a> to go back to your account.</p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto mt-5">
+            <div class="payment">
+                <div class="payment_header">
+                    <div class="check"><i class="fa fa-check" aria-hidden="true"></i></div>
+                </div>
+                <div class="content">
+                    <h1>Address Added Succesfully</h1>
+                    <p>Billing and Shipping Details Added Succesfully. <br>
+                        Let's Continue the Payment</p>
+                    <a href="cart.php">Go to Cart</a>
+                </div>
+
+            </div>
         </div>
     </div>
-    <?php include 'footer.php'; ?>
 </div>
+
+
 </body>
 </html>
