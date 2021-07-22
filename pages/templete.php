23ff442b34545ce04e1961219da2749178796a1a
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-07-22 15:43:28 +0530

    Done

---
+++ pages/templete.php
@@ -0,0 +1,7 @@
+<div>
+    <p> Thank you for contacting <strong>Orchid Bliss Support Center </strong> we will get back to you soon. <br> your details are as follows:</p>
+    <p>
+        Name: %subject% <br>
+        Message: %message%<br>
+    </p>
+</div>
