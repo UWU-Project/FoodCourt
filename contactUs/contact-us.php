7c966ada9f457e1ce392b86193042c9902c3d4c0
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-07-23 19:12:56 +0530

    Done the Validation of Contact us

--- contactUs/contact-us.php
+++ contactUs/contact-us.php
@@ -222,7 +222,7 @@ error_reporting(0);
                 ?>
                 <div class="form-group has-label">
                   <label for="validationCustom01" class="form-label ">Full Name</label>
-                  <input type="text" name="subject" id="validationCustom01" maxlength="255" class="form-control is-valid" placeholder="Full Name" required="true">
+                  <input type="text" name="subject" id="validationCustom01" maxlength="255" class="form-control " placeholder="Full Name" required="true">
                 </div>
 
                 <div class="form-group has-label">
@@ -250,88 +250,104 @@ error_reporting(0);
   </header>
   <!-- -------- END HEADER 8 w/ card over right bg image ------- -->
 
-  <footer class="footer" style="background: #383838; padding-top: 5px">
+<!-- -------- FOOTER START ------- -->
+<footer class="footer" style="background: #383838; padding-top: 5px">
     <hr class="horizontal dark mb-5">
     <div class="container">
-      <div class=" row">
-        <div class="col-md-4" style="color: #B6B6B6;">
-              <div>
-                <a href="#">
-                  <img src="../images/footerlogo5.png" alt="">
-                </a>
-
-                <p>
-                  We stand for best in everything we do, to create an environment where absolute guest satisfaction,which is our highest priority.
-
-                </p>
-              </div>
-              <div>
-                <h6 class="mt-3 mb-2 opacity-8" style="color: #fff;">Social</h6>
-                <ul class="d-flex flex-row ms-n3 nav" >
-                  <li class="nav-item" style="color: #fff;">
-                    <a class="nav-link pe-1" href="https://www.facebook.com" target="_blank">
-                      <i class="fab fa-facebook text-lg opacity-8" style="color: #fff;"></i>
+        <div class=" row">
+            <div class="col-md-4" style="color: #B6B6B6;">
+                <div>
+                    <a href="#">
+                        <img src="../images/footerlogo5.png" alt="">
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link pe-1" href="https://twitter.com" target="_blank">
-                      <i class="fab fa-twitter text-lg opacity-8" style="color: #fff;"></i>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link pe-1" href="https://dribbble.com" target="_blank">
-                      <i class="fab fa-dribbble text-lg opacity-8" style="color: #fff;"></i>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link pe-1" href="https://github.com" target="_blank">
-                      <i class="fab fa-github text-lg opacity-8" style="color: #fff;"></i>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link pe-1" href="https://www.youtube.com" target="_blank">
-                      <i class="fab fa-youtube text-lg opacity-8" style="color: #fff;"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-        </div>
+
+                    <p>
+                        We stand for best in everything we do, to create an environment where absolute guest
+                        satisfaction,which is our highest priority.
+
+                    </p>
+                </div>
+                <div>
+                    <h6 class="mt-3 mb-2 opacity-8" style="color: #fff;">Social</h6>
+                    <ul class="d-flex flex-row ms-n3 nav">
+                        <li class="nav-item" style="color: #fff;">
+                            <a class="nav-link pe-1" href="https://www.facebook.com" target="_blank">
+                                <i class="fab fa-facebook text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://twitter.com" target="_blank">
+                                <i class="fab fa-twitter text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://dribbble.com" target="_blank">
+                                <i class="fab fa-dribbble text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://github.com" target="_blank">
+                                <i class="fab fa-github text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://www.youtube.com" target="_blank">
+                                <i class="fab fa-youtube text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
 
-        <div class="col-md-5 col-sm-6 col-6 mb-4" style="color: #B6B6B6;">
-            <div class="block" >
-              <h4 style="color: #fff;">GET IN TOUCH</h4>
-              <p ><i class="fa fa-map-marker"></i> <span style="color: #fff;">&emsp;FOOD COURT:</span> NO:22 Mccallum's Drive Nuwara Eliya</p>
-              <p> <i class="fa fa-phone"></i> <span style="color: #fff;">&emsp;PHONE:</span> 052 22 22 878 </p>
+            <div class="col-md-5 col-sm-6 col-6 mb-4" style="color: #B6B6B6;">
+                <div class="block">
+                    <h4 style="color: #fff;">GET IN TOUCH</h4>
+                    <p><i class="fa fa-map-marker"></i> <span style="color: #fff;">&emsp;FOOD COURT:</span> NO:22
+                        Mccallum's Drive Nuwara Eliya</p>
+                    <p><i class="fa fa-phone"></i> <span style="color: #fff;">&emsp;PHONE:</span> 052 22 22 878 </p>
 
-              <p> <i class="fa fa-mobile"></i> <span style="color: #fff;">&emsp;MOBILE:</span> 070 2 100 600</p>
+                    <p><i class="fa fa-mobile"></i> <span style="color: #fff;">&emsp;MOBILE:</span> 070 2 100 600</p>
 
-              <p class="mail"><i class="fa fa-envelope"></i> <span style="color: #fff;">&emsp;E-MAIL:</span> info@foodcourt.com</p>
-            </div>	<!-- End Of /.block -->
-        </div>
+                    <p class="mail"><i class="fa fa-envelope"></i> <span style="color: #fff;">&emsp;E-MAIL:</span>
+                        info@foodcourt.com</p>
+                </div>    <!-- End Of /.block -->
+            </div>
 
 
+            <div class="col-md-3">
+                <div class="block">
+                    <div class="media">
+                        <h4 style="color: #fff;">OUR LOCATION</h4>
+                        <div id="map"></div>
 
-        <div class="col-md-3">
-              <div class="block">
-                <div class="media">
-                  <h4 style="color: #fff;">OUR LOCATION</h4>
-                </div>	<!-- End Of /.media -->
-              </div>	<!-- End Of /.block -->
-        </div> <!-- End Of Col-md-3 -->
 
-        <div class="col-12">
-            <div class="text-center" >
-              <p class="my-4 text-sm" style="color: #fff;">
-                 Copyright © <script>
-                  document.write(new Date().getFullYear())
-                </script> | Food Court <a href="../admin/login-form.php" target="_blank" >Administrator</a> All Rights Reserved
-              </p>
+                    </div>    <!-- End Of /.media -->
+                </div>    <!-- End Of /.block -->
+            </div> <!-- End Of Col-md-3 -->
+
+            <div class="col-12">
+                <div class="text-center">
+                    <p class="my-4 text-sm" style="color: #fff;">
+                        Copyright ©
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        | Food Court <a href="../admin/login-form.php" target="_blank">Administrator</a> All Rights
+                        Reserved
+                    </p>
+                </div>
             </div>
         </div>
-      </div>
     </div>
-  </footer>
+</footer>
+<!-- Google Map -->
+<script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqqBMyAoQe2LlTe9e3_U5O8NaUwEJ9dDU&callback=initMap&libraries=&v=weekly"
+        async
+></script>
+<script src="../validation/map.js"></script>
+<!-- Google Map End -->
 
 
 
@@ -339,8 +355,7 @@ error_reporting(0);
 
 
 
-
-  <!--   Core JS Files   -->
+<!--   Core JS Files   -->
   <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
 
   <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
@@ -355,6 +370,29 @@ error_reporting(0);
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
           crossorigin="anonymous"></script>
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function () {
+        'use strict'
+
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+                    form.classList.add('was-validated')
+                }, false)
+            })
+        }, false)
+    }())
+</script>
+
 </body>
 
 </html>
