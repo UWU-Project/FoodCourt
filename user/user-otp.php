aba408eb72f9f33fdebb43e4fb217ef3c7ae39a8
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-08-02 22:57:45 +0530

    Done the Validation of Contact us

--- user/user-otp.php
+++ user/user-otp.php
@@ -10,47 +10,272 @@ if($email == false){
 <head>
     <meta charset="UTF-8">
     <title>Code Verification</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../assets/css/soft-design-system.css?v=1.0.5" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+
+
+<!-- TOP HEADER Start
+================================================== -->
+
+<section id="top">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 offset-md-4 form">
-                <form action="user-otp.php" method="POST" autocomplete="off">
-                    <h2 class="text-center">Code Verification</h2>
-                    <?php 
-                    if(isset($_SESSION['info'])){
-                        ?>
-                        <div class="alert alert-success text-center">
-                            <?php echo $_SESSION['info']; ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                    <?php
-                    if(count($errors) > 0){
-                        ?>
-                        <div class="alert alert-danger text-center">
-                            <?php
-                            foreach($errors as $showerror){
-                                echo $showerror;
-                            }
-                            ?>
-                        </div>
-                        <?php
-                    }
-                    ?>
-                    <div class="form-group">
-                        <input class="form-control" type="number" name="otp" placeholder="Enter verification code" required>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control button" type="submit" name="check" value="Submit">
-                    </div>
-                </form>
+
+            <div class="col-md-6">
+                <p class="contact-action"><i class="fa fa-phone-square"></i>CST GROUP 4 [ FOOD COURT ]</p>
             </div>
+
+            <div class="col-md-3 clearfix">
+                <ul class="login-cart">
+                    <li>
+                        <a href="../user/login-user.php" class="active"> <i class="fas fa-user active"></i>LOGIN</a>
+                    </li>
+                    <li>
+                        <a href="../user/signup-user.php"><i class="fas fa-user-plus"></i>REGISTER</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-1">
+
+            </div>
+
+            <div class="col-md-2">
+                <div class="search-box">
+                    <div class="input-group">
+                        <input placeholder="Search Here" type="text" class="form-control">
+                        <span class="input-group-btn">
+					        	<button type="button">
+
+					      	</span>
+                    </div><!-- /.input-group -->
+                </div><!-- /.search-box -->
+            </div>
+        </div> <!-- End Of /.row -->
+    </div>	<!-- End Of /.Container -->
+
+</section>  <!-- End of /Section -->
+
+<!-- LOGO Start
+================================================== -->
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="#">
+                    <img src="../images/logo2copy.png" alt="logo">
+                </a>
+            </div>	<!-- End of /.col-md-12 -->
+        </div>	<!-- End of /.row -->
+    </div>	<!-- End of /.container -->
+</header> <!-- End of /Header -->
+
+
+<!-- Navbar -->
+<div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+        <div class="col-12">
+
+            <nav class="navbar navbar-expand-lg  blur  top-0  z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+
+                <div class="container-fluid">
+                    <a class="navbar-brand font-weight-bolder ms-sm-3" href="../index.php" rel="tooltip" title="Orchid Bliss" data-placement="bottom" target="_blank">
+                        ORCHID BLISS
+                    </a>
+
+                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon mt-2">
+                            <span class="navbar-toggler-bar bar1"></span>
+                            <span class="navbar-toggler-bar bar2"></span>
+                            <span class="navbar-toggler-bar bar3"></span>
+                          </span>
+                    </button>
+
+
+
+                    <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
+                        <ul class="nav navbar-nav nav-main">
+                            <li class="nav-item dropdown dropdown-hover mx-10">
+                            </li>
+                            <li class="nav" style="margin-left: 50px">
+                                <a class="nav-link nav-link-icon me-2 active " href="../user/login-user.php" target="_blank">
+                                    <i class="fas fa-user me-1"></i>
+                                    <p class="d-inline text-sm z-index-1 font-weight-bold">LOGIN</p>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
         </div>
     </div>
-    
+</div>
+
+<div class="page-header min-vh-50" style="margin-top: 1cm">
+    <div class="container">
+            <div class="row">
+                <div class="card d-flex blur justify-content-center p-4 shadow-lg my-sm-0 my-sm-6 mt-8 mb-5 col-md-4 offset-md-4 form">
+                    <form action="user-otp.php" method="POST" autocomplete="off">
+                        <h2 class="text-center">Code Verification</h2>
+                        <?php
+                        if(isset($_SESSION['info'])){
+                            ?>
+                            <div class="alert alert-success text-center">
+                                <?php echo $_SESSION['info']; ?>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                        <?php
+                        if(count($errors) > 0){
+                            ?>
+                            <div class="alert alert-danger text-center">
+                                <?php
+                                foreach($errors as $showerror){
+                                    echo $showerror;
+                                }
+                                ?>
+                            </div>
+                            <?php
+                        }
+                        ?>
+                        <div class="form-group">
+                            <input class="form-control" type="number" name="otp" placeholder="Enter verification code" required>
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control button bg-warning btn-lg w-100 mt-3 mb-0" type="submit" name="check" value="Submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+</div>
+
+    <!-- -------- FOOTER START ------- -->
+    <footer class="footer" style="background: #383838; padding-top: 5px">
+        <hr class="horizontal dark mb-5">
+        <div class="container">
+            <div class=" row">
+                <div class="col-md-4" style="color: #B6B6B6;">
+                    <div>
+                        <a href="#">
+                            <img src="../images/footerlogo5.png" alt="">
+                        </a>
+
+                        <p>
+                            We stand for best in everything we do, to create an environment where absolute guest
+                            satisfaction,which is our highest priority.
+
+                        </p>
+                    </div>
+                    <div>
+                        <h6 class="mt-3 mb-2 opacity-8" style="color: #fff;">Social</h6>
+                        <ul class="d-flex flex-row ms-n3 nav">
+                            <li class="nav-item" style="color: #fff;">
+                                <a class="nav-link pe-1" href="https://www.facebook.com" target="_blank">
+                                    <i class="fab fa-facebook text-lg opacity-8" style="color: #fff;"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://twitter.com" target="_blank">
+                                    <i class="fab fa-twitter text-lg opacity-8" style="color: #fff;"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://dribbble.com" target="_blank">
+                                    <i class="fab fa-dribbble text-lg opacity-8" style="color: #fff;"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://github.com" target="_blank">
+                                    <i class="fab fa-github text-lg opacity-8" style="color: #fff;"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link pe-1" href="https://www.youtube.com" target="_blank">
+                                    <i class="fab fa-youtube text-lg opacity-8" style="color: #fff;"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div class="col-md-5 col-sm-6 col-6 mb-4" style="color: #B6B6B6;">
+                    <div class="block">
+                        <h4 style="color: #fff;">GET IN TOUCH</h4>
+                        <p><i class="fa fa-map-marker"></i> <span style="color: #fff;">&emsp;FOOD COURT:</span> NO:22
+                            Mccallum's Drive Nuwara Eliya</p>
+                        <p><i class="fa fa-phone"></i> <span style="color: #fff;">&emsp;PHONE:</span> 052 22 22 878 </p>
+
+                        <p><i class="fa fa-mobile"></i> <span style="color: #fff;">&emsp;MOBILE:</span> 070 2 100 600</p>
+
+                        <p class="mail"><i class="fa fa-envelope"></i> <span style="color: #fff;">&emsp;E-MAIL:</span>
+                            info@foodcourt.com</p>
+                    </div>    <!-- End Of /.block -->
+                </div>
+
+
+                <div class="col-md-3">
+                    <div class="block">
+                        <div class="media">
+                            <h4 style="color: #fff;">OUR LOCATION</h4>
+                            <div id="map"></div>
+
+
+                        </div>    <!-- End Of /.media -->
+                    </div>    <!-- End Of /.block -->
+                </div> <!-- End Of Col-md-3 -->
+
+                <div class="col-12">
+                    <div class="text-center">
+                        <p class="my-4 text-sm" style="color: #fff;">
+                            Copyright ©
+                            <script>
+                                document.write(new Date().getFullYear())
+                            </script>
+                            | Food Court <a href="../admin/login-form.php" target="_blank">Administrator</a> All Rights
+                            Reserved
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Google Map -->
+    <script
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqqBMyAoQe2LlTe9e3_U5O8NaUwEJ9dDU&callback=initMap&libraries=&v=weekly"
+            async
+    ></script>
+    <script src="../validation/map.js"></script>
+    <!-- Google Map End -->
+
+
+
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+    <script src="../assets/js/plugins/parallax.min.js"></script>
+    <!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc -->
+
+    <script src="../assets/js/soft-design-system.min.js?v=1.0.5" type="text/javascript"></script>
+
 </body>
 </html>
