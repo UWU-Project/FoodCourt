f0c86bbc9eb99c85ba7e427fcd3b7e1d5b307f60
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-08-05 22:32:40 +0530

    Linked Contact us

--- TableBook/reserve.php
+++ TableBook/reserve.php
@@ -47,8 +47,10 @@ or die("Something is wrong ... \n" . mysqli_error());
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
+    <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
@@ -66,7 +68,7 @@ or die("Something is wrong ... \n" . mysqli_error());
     <link rel="stylesheet" href="../css/style.css">
 
     <script language="JavaScript" src="../validation/user.js"></script>
-
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
 
@@ -212,7 +214,7 @@ or die("Something is wrong ... \n" . mysqli_error());
 
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog  d-flex blur justify-content-center p-4 shadow-lg my-sm-0 my-sm-6 mt-8 mb-5 " role="document">
+            <div class="modal-dialog" role="document">
                 <div class="modal-content d-flex blur justify-content-center p-4 shadow-lg my-sm-0 my-sm-6 mt-8 mb-5 ">
                     <div class="modal-header ">
                         <h5 class="modal-title font-weight-bolder text-primary text-gradient" id="exampleModalLabel">Reserve A Table</h5>
--- index.php
+++ index.php
@@ -34,38 +34,25 @@ if (isset($_POST['Submit'])){
 	<title><?php echo APP_NAME; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-	<!-- Fonts -->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700' rel='stylesheet' type='text/css'>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
-    <!-- Icon -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
 
-	<!-- Css -->
-	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" />
-	<link rel="stylesheet" href="css/owl.carousel.css">
-	<link rel="stylesheet" href="css/owl.theme.css">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/responsive.css">
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
 
-	<!-- jS -->
-	<script src="js/jquery.min.js" type="text/javascript"></script>
-	<script src="js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
-	<script src="js/owl.carousel.min.js" type="text/javascript"></script>
-	<script src="js/jquery.nicescroll.js"></script>
-	<script src="js/jquery.scrollUp.min.js"></script>
-	<script src="js/main.js" type="text/javascript"></script>
+    <!-- CSS Files -->
+    <link id="pagestyle" href="assets/css/soft-design-system.css?v=1.0.5" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css">
 
-    <!--   Core JS Files   -->
-    <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
-    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+
     <!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
     <script src="assets/js/plugins/countup.min.js"></script>
-    <!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
-    <script src="assets/js/plugins/parallax.min.js"></script>
+
 
     <style>
 	.txtpadding{
@@ -278,42 +265,28 @@ body{
 
 
 <!-- TOP HEADER Start
-    ================================================== -->
+================================================== -->
 
 <section id="top">
     <div class="container">
         <div class="row">
-            <div class="col-md-5">
+
+            <div class="col-md-6">
                 <p class="contact-action"><i class="fa fa-phone-square"></i>CST GROUP 4 [ FOOD COURT ]</p>
             </div>
-            <div class="col-md-5 clearfix">
+
+            <div class="col-md-3 clearfix">
                 <ul class="login-cart">
                     <li>
-                        <a href="customer/login.php">LOGIN</a>
+                        <a href="user/login-user.php"> <i class="fas fa-user"></i>LOGIN</a>
                     </li>
                     <li>
-                        <a href="customer/create.php">REGISTER</a>
-                    </li>
-                    <li>
-                        <div class="cart dropdown">
-                            <a data-toggle="dropdown" href="#"><i class="fa fa-shopping-cart"></i>Cart(1)</a>
-                            <div class="dropdown-menu dropup">
-                                <span class="caret"></span>
-                                <ul class="media-list">
-                                    <li class="media">
-                                        <img class="pull-left" src="images/b2.jpg" alt="">
-                                        <div class="media-body">
-                                            <h6 style="color: #1e1717">Sri Lankan Kottu
-                                                <span>$250</span>
-                                            </h6>
-                                        </div>
-                                    </li>
-                                </ul>
-                                <button class="btn btn-primary btn-sm">Checkout</button>
-                            </div>
-                        </div>
+                        <a href="user/signup-user.php"><i class="fas fa-user-plus"></i>REGISTER</a>
                     </li>
                 </ul>
+            </div>
+            <div class="col-md-1">
+
             </div>
 
             <div class="col-md-2">
@@ -321,30 +294,31 @@ body{
                     <div class="input-group">
                         <input placeholder="Search Here" type="text" class="form-control">
                         <span class="input-group-btn">
-					        	<button class="btn btn-default" type="button"></button>
+					        	<button type="button">
+
 					      	</span>
                     </div><!-- /.input-group -->
                 </div><!-- /.search-box -->
             </div>
         </div> <!-- End Of /.row -->
     </div>	<!-- End Of /.Container -->
-    </section>
-	
 
-    <!-- LOGO Start
+</section>  <!-- End of /Section -->
+
+<!-- LOGO Start
 ================================================== -->
 
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <a href="#">
-                        <img src="images/logo2copy.png" alt="logo">
-                    </a>
-                </div>	<!-- End of /.col-md-12 -->
-            </div>	<!-- End of /.row -->
-        </div>	<!-- End of /.container -->
-    </header> <!-- End of /Header -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="#">
+                    <img src="images/logo2copy.png" alt="logo">
+                </a>
+            </div>	<!-- End of /.col-md-12 -->
+        </div>	<!-- End of /.row -->
+    </div>	<!-- End of /.container -->
+</header> <!-- End of /Header -->
 
 
 <!-- Navbar -->
@@ -355,7 +329,7 @@ body{
             <nav class="navbar navbar-expand-lg  blur  top-0  z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
 
                 <div class="container-fluid">
-                    <a class="navbar-brand font-weight-bolder ms-sm-3" href="../index.php" rel="tooltip" title="Orchid Bliss" data-placement="bottom" target="_blank">
+                    <a class="navbar-brand font-weight-bolder ms-sm-3" href="index.php" rel="tooltip" title="Orchid Bliss" data-placement="bottom" target="_blank">
                         ORCHID BLISS
                     </a>
 
@@ -370,41 +344,49 @@ body{
 
 
                     <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
-                        <ul class="nav navbar-nav nav-main mx-auto"
-                            <li class="nav-item dropdown dropdown-hover px-2" >
-                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center "  href="../pastry-shop.php">
+                        <ul class="nav navbar-nav nav-main">
+                            <li class="nav-item dropdown dropdown-hover mx-6 " >
+
+                            </li>
+
+
+
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center "  href="pastry-shop.php">
                                     PASTRY SHOP
                                 </a>
                             </li>
 
-                            <li class="nav-item dropdown dropdown-hover px-2" >
-                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" href="../lounge.php" >
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" href="lounge.php" >
                                     THE LOUNGE
                                 </a>
                             </li>
 
-                            <li class="nav-item dropdown dropdown-hover px-2" >
-                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"  href="../TableBook/buffet.php">
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"  href="TableBook/TBB.php">
                                     BUFFET
                                 </a>
                             </li>
 
-                            <li class="nav-item dropdown dropdown-hover px-2" >
-                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"  aria-selected="true" href="../about-us-old.php">
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"  aria-selected="true" href="about-us.php">
                                     ABOUT US
                                 </a>
                             </li>
 
-
-                            <li class="nav-item dropdown dropdown-hover px-2" >
-                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center active"  aria-selected="true" href="../about-us-old.php">
+                            </li>
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center active"  aria-selected="true" href="contactUs/contact-us.php">
                                     CONTACT US
                                 </a>
                             </li>
+                            <li class="nav-item dropdown dropdown-hover mx-6" >
 
+                            </li>
 
-                            <li class="nav ms-auto" >
-                                <a class="nav-link nav-link-icon me-2 " href="../cart/cart.php" target="_blank" >
+                            <li class="nav" style="margin-left: 50px">
+                                <a class="nav-link nav-link-icon me-2 " href="cart/cart.php" target="_blank" >
 
                                     <i class="fa fa-shopping-cart me-1"></i>
                                     <p class="d-inline text-sm z-index-1 font-weight-bold" >CART</p>
@@ -796,96 +778,125 @@ body{
 			</div>	<!-- End of /.row -->
 		</div>	<!-- End of /.container -->
 	</section>	<!-- End of Section -->
-	
-
-	<!-- FOOTER Start
-    ================================================== -->
-
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-					<div class="block clearfix">
-						<a href="#">
-							<img src="images/footerlogo5.png" alt="">
-						</a>
-                        <br><br>
-						<p>
-                            We stand for best in everything we do, to create an environment where absolute guest satisfaction,which is our highest priority.
-
-                        </p>
-						<h4 class="connect-heading">CONNECT WITH US</h4>
-						<ul class="social-icon">
-							<li>
-								<a class="facebook-icon" href="#">
-									<i class="fa fa-facebook"></i>
-								</a>
-							</li>
-							<li>
-								<a class="plus-icon" href="#">
-									<i class="fa fa-google-plus"></i>
-								</a>
-							</li>
-							<li>
-								<a class="twitter-icon" href="#">
-									<i class="fa fa-twitter"></i>
-								</a>
-							</li>
-							<li>
-								<a class="pinterest-icon" href="#">
-									<i class="fa fa-pinterest"></i>
-								</a>
-							</li>
-							<li>
-								<a class="linkedin-icon" href="#">
-									<i class="fa fa-linkedin"></i>
-								</a>
-							</li>
-						</ul>	<!-- End Of /.social-icon -->
-					</div>	<!-- End Of /.block -->
-				</div> <!-- End Of /.Col-md-4 -->
-				<div class="col-md-4">
-					<div class="block">
-						<h4>GET IN TOUCH</h4>
-						<p ><i class="fa  fa-map-marker"></i> <span>Food Court: </span>NO:22 Mccallum's Drive Nuwara Eliya</p>
-						<p> <i class="fa  fa-phone"></i> <span>Phone:</span> 052 22 22 878 </p>
-
-						<p> <i class="fa  fa-mobile"></i> <span>Mobile:</span> 070 2 100 600</p>
- 
-						<p class="mail"><i class="fa  fa-envelope"></i>Eamil: <span>info@foodcourt.com</span></p>
-					</div>	<!-- End Of /.block -->
-				</div> <!-- End Of Col-md-3 -->
-
-				<div class="col-md-4">
-					<div class="block">
-						<div class="media">
-						<h4>Our Location</h4>
 
 
-						</div>	<!-- End Of /.media -->
-					</div>	<!-- End Of /.block -->
-				</div> <!-- End Of Col-md-3 -->
-			</div> <!-- End Of /.row -->
-		</div> <!-- End Of /.Container -->
-		
+<!-- -------- FOOTER START ------- -->
+<footer class="footer" style="background: #383838; padding-top: 5px">
+    <hr class="horizontal dark mb-5">
+    <div class="container">
+        <div class=" row">
+            <div class="col-md-4" style="color: #B6B6B6;">
+                <div>
+                    <a href="#">
+                        <img src="images/footerlogo5.png" alt="">
+                    </a>
+
+                    <p>
+                        We stand for best in everything we do, to create an environment where absolute guest
+                        satisfaction,which is our highest priority.
+
+                    </p>
+                </div>
+                <div>
+                    <h6 class="mt-3 mb-2 opacity-8" style="color: #fff;">Social</h6>
+                    <ul class="d-flex flex-row ms-n3 nav">
+                        <li class="nav-item" style="color: #fff;">
+                            <a class="nav-link pe-1" href="https://www.facebook.com" target="_blank">
+                                <i class="fab fa-facebook text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://twitter.com" target="_blank">
+                                <i class="fab fa-twitter text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://dribbble.com" target="_blank">
+                                <i class="fab fa-dribbble text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://github.com" target="_blank">
+                                <i class="fab fa-github text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pe-1" href="https://www.youtube.com" target="_blank">
+                                <i class="fab fa-youtube text-lg opacity-8" style="color: #fff;"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
 
 
-	<!-- FOOTER-BOTTOM Start
-    ================================================== -->
+            <div class="col-md-5 col-sm-6 col-6 mb-4" style="color: #B6B6B6;">
+                <div class="block">
+                    <h4 style="color: #fff;">GET IN TOUCH</h4>
+                    <p><i class="fa fa-map-marker"></i> <span style="color: #fff;">&emsp;FOOD COURT:</span> NO:22
+                        Mccallum's Drive Nuwara Eliya</p>
+                    <p><i class="fa fa-phone"></i> <span style="color: #fff;">&emsp;PHONE:</span> 052 22 22 878 </p>
 
-		<div class="footer-bottom">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						
-						<p style="text-align: center;">© 2021 | Food Court <a href="admin/login-form.php">Administrator</a> All Rights Reserved</p>
-					</div>	<!-- End Of /.col-md-12 -->	
-				</div>	<!-- End Of /.row -->	
-			</div>	<!-- End Of /.container -->	
-		</div>	<!-- End Of /.footer-bottom -->
-	</footer> <!-- End Of Footer -->
-	
-	<a id="back-top" href="#"></a>
+                    <p><i class="fa fa-mobile"></i> <span style="color: #fff;">&emsp;MOBILE:</span> 070 2 100 600</p>
+
+                    <p class="mail"><i class="fa fa-envelope"></i> <span style="color: #fff;">&emsp;E-MAIL:</span>
+                        info@foodcourt.com</p>
+                </div>    <!-- End Of /.block -->
+            </div>
+
+
+            <div class="col-md-3">
+                <div class="block">
+                    <div class="media">
+                        <h4 style="color: #fff;">OUR LOCATION</h4>
+                        <div id="map"></div>
+
+
+                    </div>    <!-- End Of /.media -->
+                </div>    <!-- End Of /.block -->
+            </div> <!-- End Of Col-md-3 -->
+
+            <div class="col-12">
+                <div class="text-center">
+                    <p class="my-4 text-sm" style="color: #fff;">
+                        Copyright ©
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                        | Food Court <a href="admin/login-form.php" target="_blank">Administrator</a> All Rights
+                        Reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Google Map -->
+<script
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqqBMyAoQe2LlTe9e3_U5O8NaUwEJ9dDU&callback=initMap&libraries=&v=weekly"
+        async
+></script>
+<script src="validation/map.js"></script>
+<!-- Google Map End -->
+
+<!--   Core JS Files   -->
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+
+<script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+<!--  Plugin for Parallax, full documentation here: https://github.com/wagerfield/parallax  -->
+<script src="assets/js/plugins/parallax.min.js"></script>
+<!-- Control Center for Soft UI Kit: parallax effects, scripts for the example pages etc -->
+
+<script src="assets/js/soft-design-system.min.js?v=1.0.5" type="text/javascript"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+
 </body>
 </html>
 
