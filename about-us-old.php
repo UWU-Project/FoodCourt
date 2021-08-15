b33784d3069faf0c22e299f96319a7341df00ac4
AkilaGunasekara <cst18022@std.uwu.ac.lk>
2021-08-15 13:09:51 +0530

    About us new

--- about-us-old.php
+++ about-us-old.php
@@ -1,9 +1,3 @@
-<?php
-//checking connection and connecting to a database
-require_once('connection/config.php');
-//Connect to mysqli server
-?>
-
 
 <!doctype html>
 <html lang="en">
@@ -12,35 +6,98 @@ require_once('connection/config.php');
     <title>Food  Court</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <!-- Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700' rel='stylesheet' type='text/css'>
-    <!-- Icon -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Css -->
-    <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" />
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
-    <!-- jS -->
-    <script src="js/jquery.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
-    <script src="js/owl.carousel.min.js" type="text/javascript"></script>
-    <script src="js/jquery.nicescroll.js"></script>
-    <script src="js/jquery.scrollUp.min.js"></script>
-    <script src="js/main.js" type="text/javascript"></script>
-    <script language="JavaScript" src="validation/user.js"></script>
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <!-- CSS Files -->
+    <link id="pagestyle" href="assets/css/soft-design-system.css?v=1.0.5" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
+    <script src="assets/js/plugins/countup.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
 
     <style>
-        .view1{
+        * {box-sizing: border-box;}
 
-            margin: 15px;
-            width: 200px;
-            float: left;
+        .mySlides {display: none;}
+        img {vertical-align: middle;}
+
+        /* Slideshow container */
+        .slideshow-container {
+            max-width: 1000px;
+            position: relative;
+            margin: auto;
+        }
+
+        /* Caption text */
+        .text {
+            color: #f2f2f2;
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+        }
+
+        /* Number text (1/3 etc) */
+        .numbertext {
+            color: #f2f2f2;
+            font-size: 12px;
+            padding: 8px 12px;
+            position: absolute;
+            top: 0;
+        }
+
+        /* The dots/bullets/indicators */
+        .dot {
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #ecd92a;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+        }
+
+        .active1 {
+            background-color: #d2d0d0;
+        }
+
+        /* Fading animation */
+        .fade {
+            -webkit-animation-name: fade;
+            -webkit-animation-duration: 5s;
+            animation-name: fade;
+            animation-duration: 5s;
+        }
+
+        @-webkit-keyframes fade {
+            from {opacity: .4}
+            to {opacity: 1}
+        }
+
+        @keyframes fade {
+            from {opacity: .4}
+            to {opacity: 1}
+        }
+
+        /* On smaller screens, decrease text size */
+        @media only screen and (max-width: 300px) {
+            .text {font-size: 11px}
+        }
+        body{
+            background: url(images/ourback.jpg);
+
         }
     </style>
 
@@ -55,18 +112,23 @@ require_once('connection/config.php');
 <section id="top">
     <div class="container">
         <div class="row">
-            <div class="col-md-7">
+
+            <div class="col-md-6">
                 <p class="contact-action"><i class="fa fa-phone-square"></i>CST GROUP 4 [ FOOD COURT ]</p>
             </div>
+
             <div class="col-md-3 clearfix">
                 <ul class="login-cart">
                     <li>
-                        <a href="customer/login.php">LOGIN</a>
+                        <a href="../user/login-user.php"> <i class="fas fa-user"></i>LOGIN</a>
                     </li>
                     <li>
-                        <a href="customer/create.php">REGISTER</a>
+                        <a href="../user/signup-user.php"><i class="fas fa-user-plus"></i>REGISTER</a>
                     </li>
                 </ul>
+            </div>
+            <div class="col-md-1">
+
             </div>
 
             <div class="col-md-2">
@@ -74,7 +136,8 @@ require_once('connection/config.php');
                     <div class="input-group">
                         <input placeholder="Search Here" type="text" class="form-control">
                         <span class="input-group-btn">
-					        	<button class="btn btn-default" type="button"></button>
+					        	<button type="button">
+
 					      	</span>
                     </div><!-- /.input-group -->
                 </div><!-- /.search-box -->
@@ -84,7 +147,6 @@ require_once('connection/config.php');
 
 </section>  <!-- End of /Section -->
 
-
 <!-- LOGO Start
 ================================================== -->
 
@@ -93,43 +155,96 @@ require_once('connection/config.php');
         <div class="row">
             <div class="col-md-12">
                 <a href="#">
-                    <img src="images/logo2copy.png" alt="logo">
+                    <img src="images/logo2copy.png"  class="img-fluid" alt="logo">
                 </a>
             </div>	<!-- End of /.col-md-12 -->
         </div>	<!-- End of /.row -->
     </div>	<!-- End of /.container -->
 </header> <!-- End of /Header -->
 
-<!-- MENU Start
+
+<!-- Navbar -->
+<div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+        <div class="col-12">
+
+            <nav class="navbar navbar-expand-lg  blur  top-0  z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+
+                <div class="container-fluid">
+                    <a class="navbar-brand font-weight-bolder ms-sm-3" href="../index.php" rel="tooltip" title="Orchid Bliss" data-placement="bottom" target="_blank">
+                        ORCHID BLISS
+                    </a>
+
+                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon mt-2">
+                            <span class="navbar-toggler-bar bar1"></span>
+                            <span class="navbar-toggler-bar bar2"></span>
+                            <span class="navbar-toggler-bar bar3"></span>
+                          </span>
+                    </button>
+
+
+
+                    <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
+                        <ul class="nav navbar-nav nav-main">
+                            <li class="nav-item dropdown dropdown-hover mx-6 " >
+
+                            </li>
+
+
+
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"  href="pastry-shop.php">
+                                    PASTRY SHOP
+                                </a>
+                            </li>
+
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center" href="lounge.php" >
+                                    THE LOUNGE
+                                </a>
+                            </li>
+
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"  href="TableBook/TBB.php">
+                                    BUFFET
+                                </a>
+                            </li>
+
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center active"  aria-selected="true" href="about-us.php">
+                                    ABOUT US
+                                </a>
+                            </li>
+
+                            <li class="nav-item dropdown dropdown-hover mx-2" >
+                                <a class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center "  aria-selected="true" href="contactUs/contact-us.php">
+                                    CONTACT US
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown dropdown-hover mx-6" >
+
+                            </li>
+
+                            <li class="nav" style="margin-left: 50px">
+                                <a class="nav-link nav-link-icon me-2 " href="cart/cart.php" target="_blank" >
+
+                                    <i class="fa fa-shopping-cart me-1"></i>
+                                    <p class="d-inline text-sm z-index-1 font-weight-bold" >CART</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
+        </div>
+    </div>
+</div>
+
+<!-- AAA
 ================================================== -->
-
-<nav class="navbar navbar-default">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div> <!-- End of /.navbar-header -->
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav nav-main">
-                <li ><a href="index.php">HOME</a></li>
-                <li><a href="pastry-shop.php">PASTRY SHOP</a></li>
-                <li><a href="lounge.php">THE LOUNGE</a></li>
-                <li><a href="TableBook/buffet.php">BUFFET</a></li>
-                <li class="active"><a href="about-us-old.php">ABOUT US</a></li>
-                <li ><a href="#">CART</a></li>
-            </ul>
-            </li> <!-- End of /.dropdown -->
-
-
-            </ul> <!-- End of /.nav-main -->
-        </div>	<!-- /.navbar-collapse -->
-    </div>	<!-- /.container-fluid -->
-</nav>	<!-- End of /.nav -->
+<br><br><br><br>
 
 
 
@@ -137,7 +252,7 @@ require_once('connection/config.php');
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
-					<h1>About Us</h1>
+					<h3>About Us</h3>
 				</div>	<!-- End of /.col-md-4 -->	
 
 			</div>	<!-- End of /.row -->
@@ -155,7 +270,58 @@ require_once('connection/config.php');
 					        	<i class="fa  fa-pencil"></i>
 					        </div>
 					        <div class="blog-box">
-					        	<img src="images/ABOUT.jpg" alt="">
+
+
+                                <div class="slideshow-container">
+
+                                    <div class="mySlides fade">
+                                        <div class="numbertext">1 / 3</div>
+                                        <img src="images/about1.jpg" style="width:100%">
+                                        <div class="text">Caption Text</div>
+                                    </div>
+
+                                    <div class="mySlides fade">
+                                        <div class="numbertext">2 / 3</div>
+                                        <img src="images/about2.jpg" style="width:100%">
+                                        <div class="text">Caption Two</div>
+                                    </div>
+
+                                    <div class="mySlides fade">
+                                        <div class="numbertext">3 / 3</div>
+                                        <img src="images/about3.jpg" style="width:100%">
+                                        <div class="text">Caption Three</div>
+                                    </div>
+
+                                </div>
+                                <br>
+
+                                <div style="text-align:center">
+                                    <span class="dot"></span>
+                                    <span class="dot"></span>
+                                    <span class="dot"></span>
+                                </div>
+
+                                <script>
+                                    var slideIndex = 0;
+                                    showSlides();
+
+                                    function showSlides() {
+                                        var i;
+                                        var slides = document.getElementsByClassName("mySlides");
+                                        var dots = document.getElementsByClassName("dot");
+                                        for (i = 0; i < slides.length; i++) {
+                                            slides[i].style.display = "none";
+                                        }
+                                        slideIndex++;
+                                        if (slideIndex > slides.length) {slideIndex = 1}
+                                        for (i = 0; i < dots.length; i++) {
+                                            dots[i].className = dots[i].className.replace(" active1", "");
+                                        }
+                                        slides[slideIndex-1].style.display = "block";
+                                        dots[slideIndex-1].className += " active1";
+                                        setTimeout(showSlides, 5000); // Change image every 2 seconds
+                                    }
+                                </script>
 					            
 					            <div class="blog-post-body clearfix">
 						            <a href="blog-single.html">
