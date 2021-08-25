f2da9fcfbbd2f73f2979846758fdc892b3ffe692
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2021-08-25 20:45:17 +0530

    Linked Contact us

--- admin-copy/profile.php
+++ admin-copy/profile.php
@@ -37,6 +37,13 @@ or die("There are no records to count ... \n" . mysqli_error());
   <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="./assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+    <script language="JavaScript" src="validation/admin.js"></script>
+
+    <script
+            src="https://code.jquery.com/jquery-3.6.0.min.js"
+            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+            crossorigin="anonymous"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body class="g-sidenav-show  bg-gray-100">
