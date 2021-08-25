d312a5d6988920bba94805b19b9bc988511566c2
AkilaGunasekara <cst18022@std.uwu.ac.lk>
2021-08-25 20:30:23 +0530

    Admin interfaces

--- admin-copy/user/user-otp-sucess.php
+++ admin-copy/user/user-otp-sucess.php
@@ -22,6 +22,12 @@ if($_SESSION['info'] == false){
     <!-- CSS Files -->
     <link id="pagestyle" href="../../assets/css/soft-design-system.css?v=1.0.5" rel="stylesheet" />
     <link rel="stylesheet" href="../../css/style.css">
+    <style>
+        body{
+            background: url(../../images/ourback.jpg);
+
+        }
+    </style>
 </head>
 <body>
 
@@ -147,105 +153,6 @@ if($_SESSION['info'] == false){
         </div>
     </div>
 </div>
-
-<!-- -------- FOOTER START ------- -->
-<footer class="footer" style="background: #383838; padding-top: 5px">
-    <hr class="horizontal dark mb-5">
-    <div class="container">
-        <div class=" row">
-            <div class="col-md-4" style="color: #B6B6B6;">
-                <div>
-                    <a href="#">
-                        <img src="../../images/footerlogo5.png" alt="">
-                    </a>
-
-                    <p>
-                        We stand for best in everything we do, to create an environment where absolute guest
-                        satisfaction,which is our highest priority.
-
-                    </p>
-                </div>
-                <div>
-                    <h6 class="mt-3 mb-2 opacity-8" style="color: #fff;">Social</h6>
-                    <ul class="d-flex flex-row ms-n3 nav">
-                        <li class="nav-item" style="color: #fff;">
-                            <a class="nav-link pe-1" href="https://www.facebook.com" target="_blank">
-                                <i class="fab fa-facebook text-lg opacity-8" style="color: #fff;"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://twitter.com" target="_blank">
-                                <i class="fab fa-twitter text-lg opacity-8" style="color: #fff;"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://dribbble.com" target="_blank">
-                                <i class="fab fa-dribbble text-lg opacity-8" style="color: #fff;"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://github.com" target="_blank">
-                                <i class="fab fa-github text-lg opacity-8" style="color: #fff;"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://www.youtube.com" target="_blank">
-                                <i class="fab fa-youtube text-lg opacity-8" style="color: #fff;"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-
-            <div class="col-md-5 col-sm-6 col-6 mb-4" style="color: #B6B6B6;">
-                <div class="block">
-                    <h4 style="color: #fff;">GET IN TOUCH</h4>
-                    <p><i class="fa fa-map-marker"></i> <span style="color: #fff;">&emsp;FOOD COURT:</span> NO:22
-                        Mccallum's Drive Nuwara Eliya</p>
-                    <p><i class="fa fa-phone"></i> <span style="color: #fff;">&emsp;PHONE:</span> 052 22 22 878 </p>
-
-                    <p><i class="fa fa-mobile"></i> <span style="color: #fff;">&emsp;MOBILE:</span> 070 2 100 600</p>
-
-                    <p class="mail"><i class="fa fa-envelope"></i> <span style="color: #fff;">&emsp;E-MAIL:</span>
-                        info@foodcourt.com</p>
-                </div>    <!-- End Of /.block -->
-            </div>
-
-
-            <div class="col-md-3">
-                <div class="block">
-                    <div class="media">
-                        <h4 style="color: #fff;">OUR LOCATION</h4>
-                        <div id="map"></div>
-
-
-                    </div>    <!-- End Of /.media -->
-                </div>    <!-- End Of /.block -->
-            </div> <!-- End Of Col-md-3 -->
-
-            <div class="col-12">
-                <div class="text-center">
-                    <p class="my-4 text-sm" style="color: #fff;">
-                        Copyright ©
-                        <script>
-                            document.write(new Date().getFullYear())
-                        </script>
-                        | Food Court <a href="../../admin/login-form.php" target="_blank">Administrator</a> All Rights
-                        Reserved
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- Google Map -->
-<script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqqBMyAoQe2LlTe9e3_U5O8NaUwEJ9dDU&callback=initMap&libraries=&v=weekly"
-        async
-></script>
-<script src="../../validation/map.js"></script>
-<!-- Google Map End -->
 
 
 
